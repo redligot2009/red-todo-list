72efd6315d34a26352fe571bd461e0d309330866
red.ligot <redligot2009.forreal@gmail.com>
2021-01-20 02:39:49 +0800

    Update http-common.js

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://192.168.86.165:3000/api",
+    baseURL: ()=>{
+        if(process.env.NODE_ENV=="production")
+        {
+            "https://192.168.86.165:3000/api"
+        }
+        else
+        {
+            "https://red-todo-list.herokuapp.com/api"
+        }
+    },
     headers: {
         "Content-type": "application/json"
     }
