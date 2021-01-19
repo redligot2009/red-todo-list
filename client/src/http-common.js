590a868bf2cef02c3f7735b939ee81c28e68d6c1
red.ligot <redligot2009.forreal@gmail.com>
2021-01-20 02:48:14 +0800

    Revert "Update http-common.js"
    This reverts commit 648ab7bd3215a188c061a91d394ab500f67d5f82.

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -4,11 +4,11 @@ export default axios.create({
     baseURL: ()=>{
         if(process.env.NODE_ENV=="production")
         {
-            return ("https://192.168.86.165:3000/api");
+            "https://192.168.86.165:3000/api"
         }
         else
         {
-            return ("https://red-todo-list.herokuapp.com/api");
+            "https://red-todo-list.herokuapp.com/api"
         }
     },
     headers: {
